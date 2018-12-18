7c3c8b7ead43805938fb982d08f83dfc974e676e
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-18 20:50:12 +0100

    Initial creation and added vehicle class

---
+++ Military/Military/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Military
+{
+    public abstract class Vehicle
+    {
+        public int Id { get; set; }
+        public double Weight { get; set; }
+        public double AverageSpeed { get; set; }
+        public static int FuelConsumption { get; set; }
+        public static int Capacity { get; set; }
+
+        public Vehicle(int id, double weight, double averageSpeed)
+        {
+            Id = id;
+            Weight = weight;
+            AverageSpeed = averageSpeed;
+        }
+
+        protected virtual void Print()
+        {
+            Console.WriteLine($"Id: {Id}, weight: {Weight}, average speed: {AverageSpeed}");
+        }
+    }
+}
