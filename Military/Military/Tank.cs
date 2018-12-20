410100c9cfa0d560739ff1d107d533dfd0727b53
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-20 18:44:36 +0100

    Created Tank class

---
+++ Military/Military/Tank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Military
+{
+    public class Tank : Vehicle
+    {
+        public Tank(Guid id, double weight, double averageSpeed) : base(id, weight, averageSpeed)
+        {
+            Capacity = 6;
+            FuelConsumption = 30;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"total fuel used: ");
+        }
+
+    }
+}
--- Military/Military/Vehicle.cs
+++ Military/Military/Vehicle.cs
@@ -6,22 +6,22 @@ namespace Military
 {
     public abstract class Vehicle
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public double Weight { get; set; }
         public double AverageSpeed { get; set; }
         public static int FuelConsumption { get; set; }
         public static int Capacity { get; set; }
 
-        public Vehicle(int id, double weight, double averageSpeed)
+        public Vehicle(Guid id, double weight, double averageSpeed)
         {
             Id = id;
             Weight = weight;
             AverageSpeed = averageSpeed;
         }
 
-        protected virtual void Print()
+        public virtual void Print()
         {
-            Console.WriteLine($"Id: {Id}, weight: {Weight}, average speed: {AverageSpeed}");
+            Console.WriteLine($"Id: {Id}, weight: {Weight}, average speed: {AverageSpeed},fuel consumption: {FuelConsumption}, capcaity: {Capacity}, ");
         }
     }
 }
